d31aa97f06736b381efa1d106d1b8bcb5ef4d291
jjjon-the-great <jonathan.gontmakher@gmail.com>
2026-03-09 13:40:22 +0200

    added trivial solver, starting to test in main.py

--- src/SAT.py
+++ src/SAT.py
@@ -1,3 +1,5 @@
+from typing import Generator, NewType
+
 """
 SAT problem
 """
@@ -7,18 +9,70 @@ class SAT:
             self.pos_variables = pos_variables
             self.neg_variables = neg_variables
 
-
     def __init__(self, num_variables):
         self.num_variables = num_variables
         self.clauses = []
     
     def initializeFromInput(self):
         pass
-
+    
+    #Add new clause
     def addClause(self, pos_variables, neg_variables):
         self.clauses.append(self.Clause(pos_variables, neg_variables))
 
     # returns an array of booleans containing a satisfying solution, or None if unsat
     def solve(self):
-        pass
-
+        #TODO: add better solver
+        return self.trivial_solve()
+    
+    #trivial solving of sats. iterates over all interpretations.
+    def trivial_solve(self) -> list[bool]:
+            
+        #iterates over all possible interpretations
+        def interpretation_iterator(num_variables : int) -> Generator[list[bool]]:
+            #initial zero interpretation
+            interpretation = [False]*num_variables
+            
+            #get next interpretation
+            def get_next_interpretation(interpretation : list[bool]) -> list[bool] | None:
+                # if zero not in interpretation, it is the last one.
+                if False not in interpretation:
+                    return None
+                
+                #find first zero in interpretation
+                first_false_position = interpretation.index(False)
+                
+                #construct next interpretation 
+                next_interpretation = [False] * first_false_position
+                next_interpretation.append(True)
+                next_interpretation.extend(interpretation[first_false_position + 1:])
+                return next_interpretation
+            
+            #yields interpretations
+            while interpretation:
+                yield interpretation
+                interpretation = get_next_interpretation(interpretation)
+        
+        #checks wether all clauses are satisfied
+        #Adding new type bcs pylance doens't like static types >:[
+        Clause = NewType('Clause', self.Clause) 
+        def check_interpretation_satisfaction(clauses : list[Clause], interpretation : list[bool]):
+            
+            #checks wether a single clause is satisfied
+            def check_clause_satisfaction(clause : Clause, interpretation : list[bool]):
+                
+                #checks wether at least one variable satisfies the clause
+                variable_satisfaction = [interpretation[i] for i in clause.pos_variables]
+                variable_satisfaction.extend([not interpretation[i] for i in clause.neg_variables])
+                return any(variable_satisfaction)
+            
+            #check and return all clause satisfaction
+            clauses_satisfaction = [check_clause_satisfaction(clause, interpretation) for clause in clauses]
+            return all(clauses_satisfaction)
+        
+        #finally, check all interpretations
+        for interpretation in interpretation_iterator:
+            if check_interpretation_satisfaction(interpretation, self.clauses):
+                return interpretation
+        #if none found return None
+        return None
--- src/main.py
+++ src/main.py
@@ -1,5 +1,10 @@
+from SAT import SAT
 def main():
-    pass
+    s = SAT(3)
+    s.addClause([1,2,3],[4])
+    s.addClause([2,3],[1])
+    s.addClause([])
+    s.solve()
 
 if __name__ == "__main__":
     main()
