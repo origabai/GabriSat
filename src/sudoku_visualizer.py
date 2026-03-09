deda54fa24310e049986645e79d6b8c5d5727b3a
Ori Gabai <ori.gabai07@gmail.com>
2026-03-09 17:07:40 +0200

    added documentation to sudoku_visualizer

--- src/sudoku_visualizer.py
+++ src/sudoku_visualizer.py
@@ -9,12 +9,15 @@ class Sudoku_Visualizer:
     """
     def visualize_sudoku(self, board):
         plt.cla()
+        # make axes the same scale ane make them disappear
         self.ax.set_box_aspect(1)
         self.ax.axis('off')
+        # replace None's with empty strings
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if (board[i][j] is None):
                     board[i][j] = ""
+        # plot the board
         self.ax.table(cellText=board,cellLoc='center', bbox=[0.25, 0.25, 0.5, 0.5])
         plt.show(block=False)
 
