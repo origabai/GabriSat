8ffeddb46f089ea7a768d9dbc103c50c86c574fc
Ori Gabai <ori.gabai07@gmail.com>
2026-03-09 13:40:27 +0200

    implemented reduction, and implemented generators for graph and graph coloring

--- src/graph_coloring.py
+++ src/graph_coloring.py
@@ -1,21 +1,80 @@
 from graph import Graph
+from SAT import SAT
+from random import randint
 
 class GraphColoring(Graph):
     # colors is an array representing the initial colors, or None if no color is set
-    def __init__(self, num_nodes, edges, colors):
+    def __init__(self, num_nodes: int, edges: list[list[int]], colors: list[int], max_colors: int):
         super().__init__(num_nodes, edges)
         self.colors = colors
+    
+    # generates a random graph, and random coloring
+    def generate_random(self):
+        super().generate_random()
+        self.colors = []
+        for i in range(self.num_nodes):
+            if (randint(0,1) == 0):
+                self.colors.append(None)
+            else:
+                self.colors.append(randint(0, self.max_colors-1))
 
-    def reduceToSAT(self):
-        pass
+    """
+    initalizes a graph from input in the following format:
+    num_nodes
+    num_edges
+    A[0] B[0]
+    A[1] B[1]
+    ...
+    A[num_edges-1] B[num_edges-1]
+
+    and colors in the following format:
+    C[0]
+    C[1]
+    ...
+    C[num_nodes]
+
+    where C[i] can be -1 if the node is not assigned a color
+    """
+    def generate_from_input(self):
+        super().generate_from_input()
+        self.colors = []
+        for i in range(self.num_nodes):
+            color = input(input("input color of node",i,"or -1 if unassigned: "))
+            self.colors.append(color)
 
     # returns an array of numbers representing colors of a valid coloring, or None if none exists
-    def solve(self, max_colors):
-        pass
-    
-    def initializeFromInput(self):
-        pass
+    def solve(self):
+        sat = SAT(self.max_colors * self.num_nodes)
+        # add general clauses for color relations
+        for i in range(self.num_nodes):
+            sat.addClause([i * self.max_colors + j for j in range(self.max_colors)], [])
+            for j in range(self.max_colors):
+                for k in range(j+1, self.max_colors):
+                    sat.addClause([], [i*self.max_colors + j, i*self.max_colors + k])
 
-    def generateRandom(self):
-        pass
+        # add clauses to satisfy edge constraints
+        for edge in self.edges:
+            for j in range(self.max_colors):
+                sat.addClause([], [edge[0]*self.max_colors + j, edge[1]*self.max_colors + j])
+        
+        # add clauses to satisfy inital colors
+        for i in range(self.num_nodes):
+            if self.colors[i] is None:
+                continue
+            for j in range(self.max_colors):
+                if (self.colors[i] == j):
+                    sat.addClause([i*self.max_colors + j], [])
+                else:
+                    sat.addClause([], [i*self.max_colors + j])
+
+        solution = sat.solve()
+        if (solution is None):
+            return None
+        answer = self.colors
+        for i in range(self.num_nodes):
+            for j in range(self.max_colors):
+                if (solution[i*self.max_colors + j]):
+                    answer[i] = j
+                    break
+        return answer
 
