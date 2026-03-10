f204a464c078b25c685d658cd360a0907f4c1a4a
jjjon-the-great <jonathan.gontmakher@gmail.com>
2026-03-10 15:34:20 +0200

    split the visualizer into utils

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,128 +1,58 @@
 from graph_coloring import GraphColoring
-from pyvis.network import Network
 import webbrowser
-import os
-import signal
+from graphUI_utils import GraphUtils
 
 
-from dash import Dash, html, Input, Output, State, dcc
-import dash_cytoscape as cyto
+from dash import Dash, Input, Output, State
 
+'''
+a class that shows a graph, allowing it to be edited visually.
 
+main method is .show() - which actually visualizes the graph.
+.show() returns the graph coloring problem which corresponds to what the user has edited
+in the editing window.
+
+'''
 class Visualizer:
     def __init__(self, graph : GraphColoring, solution = None):
         self.edges = graph.edges
         self.num_nodes = graph.num_nodes
-    
+        self.max_colors = graph.max_colors
+        self.color_storage_for_termination = []
+        self.COLORS = ["red", "green", "blue", "yellow", "purple", "pink", "magenta", "lime", "cyan"]
         #remembering colours
         if solution:
-            numerical_colors = solution
+            self.numerical_colors = solution
         else:
-            numerical_colors = graph.colors
+            self.numerical_colors = graph.colors
             
         #deciding visualization colours
-        self.colors = [self.color_gen(color) for color in numerical_colors]
+        self.colors = [self.color_gen(color) for color in self.numerical_colors]
         
     #to pass the test with flying colors
     def color_gen(self, color : int | None) -> str:
         if color is None:
             return "grey"
         
-        COLORS = ["red", "green", "blue", "yellow", "purple", "pink", "magenta", "lime", "cyan"]
-        return COLORS[color % len(COLORS)]
+        return self.COLORS[color % len(self.COLORS)]
     
+    #takes string returns color
+    def color_to_num(self, color : str) -> int:
+        if color == "grey":
+            return None
+        else:
+            return self.COLORS.index(color)
+        
+        
+        
     #show result
     def show(self) -> None:
-        '''
-        net = Network()
-        labels = [str(i) for i in range(self.num_nodes)]
-        net.add_nodes(list(range(self.num_nodes)), color = self.colors, label = labels)
-        net.add_edges(self.edges)
+        initial_elements = GraphUtils.generate_initial_data(self.num_nodes, self.edges, self.colors)
         
-        net.show('visual.html', notebook = False)
-        '''
-        initial_elements = []
-        for node in range(self.num_nodes):
-            initial_elements.append({'data' : {'id': str(node), 'label' : str(node), 'color': self.colors[node]}})
-        
-        for edge in self.edges:
-            initial_elements.append({'data' : {'source': str(edge[0]), 'target': str(edge[1])}})
-        
-        print(initial_elements)
-        #TODO make inital data equal to graph
-        '''
-        initial_elements = [
-            {'data': {'id': '1', 'label': 'Node A', 'color': 'blue'}},
-            {'data': {'id': '2', 'label': 'Node B', 'color': 'red'}},
-            {'data': {'source': '1', 'target': '2'}}
-        ]
-        '''
         app = Dash(__name__)
-        app.layout = html.Div([
-            html.H3("Dynamic Graph Editor"),
-            
-            dcc.Store(id="erase_toggled", storage_type='memory', data = {'toggled' : False}),
-            dcc.Store(id="color_current", storage_type='memory', data = {'colour' : None}),
-            
-            # Control Panel for Adding Nodes
-            html.Div([
-                #dcc.Input(id='input-node-id', type='text', placeholder='New Node ID (e.g., C)'),
-                #dcc.Input(id='input-node-label', type='text', placeholder='Node Label'),
-                html.Button('Add Node', id='btn-add-node', n_clicks=0, style={'backgroundColor': 'lightgray', 'color': 'black', 'padding': '10px'})
-            ], style={'marginBottom': '10px'}),
-            
-            # Control Panel for Adding Edges
-            html.Div([
-                dcc.Input(id='input-edge-source', type='text', placeholder='Source Node ID'),
-                dcc.Input(id='input-edge-target', type='text', placeholder='Target Node ID'),
-                html.Button('Add Edge', id='btn-add-edge', n_clicks=0, style={'backgroundColor': 'lightgray', 'color': 'black', 'padding': '10px'})
-            ], style={'marginBottom': '20px'}),
-
-            html.Div([
-                html.Button('Erase button', id='btn-erase', style={'backgroundColor': 'lightgray', 'color': 'black', 'padding': '10px'} ,n_clicks=0)
-            ], style={'marginBottom': '20px'}),
-            
-            html.Div([
-                html.Button('End visualization', id='btn-end', style={'backgroundColor': 'lightgray', 'color': 'black', 'padding': '10px'} ,n_clicks=0)
-            ], style={'marginBottom': '20px'}),
-            
-            html.Div([
-                html.Label("Change node colour"),
-                dcc.Dropdown(
-                    id='multi-colour-selector',
-                    options=[
-                        # 'label' is what the user sees, 'value' is what Python receives
-                        {'label': 'None (none selected)', 'value': None},
-                        {'label': 'grey (no colour)', 'value': 'grey'},
-                        {'label': 'red (0)', 'value': 'red'},
-                        {'label': 'green (1)', 'value': 'green'},
-                        {'label': 'blue (2)', 'value': 'blue'},
-                        {'label': 'yellow (3)', 'value': 'yellow'},
-                        {'label': 'purple (4)', 'value': 'purple'},
-                        {'label': 'pink (5)', 'value': 'pink'},
-                        {'label': 'magenta (6)', 'value': 'magenta'},
-                        {'label': 'lime (7)', 'value': 'lime'},
-                        {'label': 'cyan (8)', 'value': 'cyan'},
-                    ],
-                    value=[None], # The default selected array
-                    multi=False,  # This strictly enforces multiple-choice behavior
-                    style={'width': '300px', 'marginTop': '5px'}
-                )
-            ]),
-
-            # The Cytoscape Canvas
-            cyto.Cytoscape(
-                id='interactive-graph',
-                elements=initial_elements,
-                layout={'name': 'cose'}, # Force-directed physics layout
-                style={'width': '800px', 'height': '500px', 'border': '1px solid black'},
-                stylesheet=[
-                    # Basic styling to make labels visible
-                    {'selector': 'node', 'style': {'label': 'data(id)', 'text-valign': 'center', 'background-color': 'data(color)'}},
-                    {'selector': 'edge', 'style': {'curve-style': 'bezier', 'target-arrow-shape': 'none'}}
-                ]
-            )
-        ])
+        app.layout = GraphUtils.default_layout(initial_elements)
+        
+        
         @app.callback(
             Output('interactive-graph', 'elements', allow_duplicate=True),
             Input('btn-add-node', 'n_clicks'),
@@ -130,15 +60,8 @@ class Visualizer:
             prevent_initial_call=True
         )
         def add_node(n_clicks, current_elements):
-            #finds existing nodes
-            current_nodes = [int(element['data']['id']) for element in current_elements if 'target' not in element['data']]
-            #finds next node to add
-            next_id = min(set(range(1,len(current_nodes)+2))-set(current_nodes))
-            # Construct the new node dictionary and append it to the state
-            new_node = {'data': {'id': str(next_id), 'label': str(next_id)}}
-            current_elements.append(new_node)
-            
-            return current_elements
+            return GraphUtils.add_node(n_clicks, current_elements)
+
 
 
         # Callback to add a new edge
@@ -151,14 +74,9 @@ class Visualizer:
             prevent_initial_call=True
         )
         def add_edge(n_clicks, source_id, target_id, current_elements):
-            if not source_id or not target_id:
-                return current_elements # Do nothing if source/target are empty
-            
-            # Construct the new edge dictionary and append it to the state
-            new_edge = {'data': {'source': source_id, 'target': target_id}}
-            current_elements.append(new_edge)
-            
-            return current_elements
+            return GraphUtils.add_edge(n_clicks, source_id, target_id, current_elements)
+        
+        
         
         @app.callback(
             Output('erase_toggled', 'data'),
@@ -166,10 +84,9 @@ class Visualizer:
             Input('btn-erase', 'n_clicks')
         )
         def switch_erasing_mode(n_clicks):
-            if n_clicks%2 == 0:
-                return {'toggled' : False}, {'backgroundColor': 'lightgray', 'color': 'black', 'padding': '10px'}
-            else:
-                return {'toggled' : True}, {'backgroundColor': 'red', 'color': 'black', 'padding': '10px'}
+            return GraphUtils.switch_erasing_mode(n_clicks)
+        
+        
         
         @app.callback(
             Output('interactive-graph', 'elements', allow_duplicate=True),
@@ -179,92 +96,27 @@ class Visualizer:
             State('erase_toggled', 'data'),
             prevent_initial_call=True
         )
-        def process_click(tapped_node, current_elements, selected_colour ,erase_mode):
-            # Base case: The app just loaded, and no node has been clicked yet.
-            if tapped_node is None:
-                return current_elements
-            
-            if erase_mode['toggled']:
-                
-                # Extract the mathematical or topological data from the dictionary
-                node_id = tapped_node.get('id', 'Unknown')
-                #node_label = tapped_node.get('label', 'No Label')
-                # Return formatted HTML to update the DOM
-                return [element for element in current_elements if not(
-                        element['data']['id'] == node_id or
-                        ('source' in element['data'] and element['data']['source'] == node_id) or
-                        ('target' in element['data'] and element['data']['target'] == node_id))
-                        ]   
-            else:
-                if selected_colour is None:
-                    return current_elements
-                
-                # Extract the mathematical or topological data from the dictionary
-                node_id = str(tapped_node.get('id', 'Unknown'))
-                
-                elements = [element for element in current_elements if element['data']['id'] != node_id]
-                elements.append({'data' : {'id' : node_id, 'label' : node_id, 'color' : selected_colour}})
-                #node_label = tapped_node.get('label', 'No Label')
-                # Return formatted HTML to update the DOM
-                return elements  
+        def process_node_click(tapped_node, current_elements, selected_colour ,erase_mode):
+            return GraphUtils.process_node_click(tapped_node, current_elements, selected_colour ,erase_mode)
         
-        @app.callback(
-            Output('interactive-graph', 'elements', allow_duplicate=True),
-            Input('interactive-graph', 'tapEdgeData'),
-            State('interactive-graph', 'elements'),
-            State('erase_toggled', 'data'),
-            prevent_initial_call=True
-        )
-        def erase_clicked_edge(tapped_edge, current_elements, erase_mode):
-            # Base case: The app just loaded, and no node has been clicked yet.
-            if tapped_edge is None or not erase_mode['toggled']:
-                return current_elements
-            # Extract the mathematical or topological data from the dictionary
-            edge_src = tapped_edge.get('source', 'Unknown')
-            edge_target = tapped_edge.get('target', 'Unknown')
-            print(edge_src, edge_target)
-            #node_label = tapped_node.get('label', 'No Label')
-            # Return formatted HTML to update the DOM
-            return [element for element in current_elements if not(
-                    (('source' in element['data'] and element['data']['source'] == edge_src) and
-                    ('target' in element['data'] and element['data']['target'] == edge_target)) or
-                    ('source' in element['data'] and element['data']['source'] == edge_target) and
-                    ('target' in element['data'] and element['data']['target'] == edge_src))
-                    
-                    ]   
-            
+        
+        
         @app.callback(
             Input('btn-end', 'n_clicks'),
             State('interactive-graph', 'elements'),
         )
         def end_visualization(n_clicks, elements):
-            if n_clicks == 0:
-                pass
-            else:
-                print(f"I CAN ACCESS SELF!, {self.num_nodes}")
-                print(self.colors)
-                
-                #add nodes and edges
-                
-                nodes = 0
-                self.edges = []
-                for element in elements:
-                    if 'source' not in element['data']:
-                        nodes = max(nodes, int(element['data']['id']) + 1)
-                    else:
-                        self.edges.append([element['data']['source'], element['data']['target']])
-                self.num_nodes = nodes
-                
-                
-                os.kill(os.getpid(), signal.SIGINT)
+            return GraphUtils.end_visualization(n_clicks, elements, self)
         
-        
-        print("hi?")
+
+
         webbrowser.open('http://localhost:8050')
         try:
             app.run()
         except KeyboardInterrupt:
-            pass
-        print("PROGRAM TERMINATED SUCCESSFULLY!")
-        print("bi?")
+            self.color_storage_for_termination.sort(key = lambda tup : tup[0])
+            color_array = [element[1] for element in self.color_storage_for_termination]
+            
+            print(color_array)
+            return GraphColoring(self.num_nodes, self.edges, color_array, self.max_colors)
 
