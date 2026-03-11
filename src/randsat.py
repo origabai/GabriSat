61737b16c4aba1311b484c936da17206ab65c955
Ori Gabai <ori.gabai07@gmail.com>
2026-03-11 11:46:25 +0200

    documentation, oops

--- src/randsat.py
+++ src/randsat.py
@@ -1,11 +1,16 @@
 from random import randint, sample
 from SAT import DEFAULT_SOLVER
 
+"""
+generates a random SAT of type solver, with num_vars and num_clauses
+"""
 def generate_random(self, num_vars: int, num_clauses: int, solver = DEFAULT_SOLVER):
     s = solver(num_vars)
     forced_solution = [randint(0,1) for i in range(vars)]
+    # probability of 0.5 to force solvability
     force_solvable: bool = (randint(0,1) == 0)
     for j in range(num_clauses):
+        # generates a random sample of variables, and assigns them randomly to neg and pos
         clause = sample([i for i in range(num_vars)], randint(1, num_vars-1))
         pos = []
         neg = []
@@ -15,6 +20,7 @@ def generate_random(self, num_vars: int, num_clauses: int, solver = DEFAULT_SOLV
             else:
                 neg.append(x)
         y = randint(0,num_vars-1)
+        # force the clause to be satisfiable
         if (force_solvable):
             if (forced_solution[y]==0):
                 neg.append(y)
